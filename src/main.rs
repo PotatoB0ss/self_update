df590d12aab0b87ca64e6c3114901a7c23115067
PotatoPC <drafasenos317@gmail.com>
2023-08-12 15:05:37 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+
+#[macro_use]
+extern crate self_update;
+
+fn run() -> Result<(), Box<dyn ::std::error::Error>> {
+    let releases = self_update::backends::github::ReleaseList::configure()
+        .repo_owner("PotatoB0ss")
+        .repo_name("self_update")
+        .build()?
+        .fetch()?;
+    println!("found releases:");
+    println!("{:#?}\n", releases);
+
+    let status = self_update::backends::github::Update::configure()
+        .repo_owner("jaemk")
+        .repo_name("self_update")
+        .bin_name("github")
+        .show_download_progress(true)
+        //.target_version_tag("v9.9.10")
+        //.show_output(false)
+        //.no_confirm(true)
+        //
+        // For private repos, you will need to provide a GitHub auth token
+        // **Make sure not to bake the token into your app**; it is recommended
+        // you obtain it via another mechanism, such as environment variables
+        // or prompting the user for input
+        //.auth_token(env!("DOWNLOAD_AUTH_TOKEN"))
+        .current_version(cargo_crate_version!())
+        .build()?
+        .update()?;
+    println!("Update status: `{}`!", status.version());
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        println!("[ERROR] {}", e);
+        ::std::process::exit(1);
+    }
+}
